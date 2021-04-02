47f368b4367e5d68f1ac61df062c48b1bd820a49
Futursaith <saith_7@hotmail.fr>
2021-04-02 10:01:00 +0200

    info.

--- info.js
+++ info.js
@@ -1,55 +1,106 @@
+/*
+Créer un object literal alice qui contiendra les informations suivantes:
+firstName: 'Alice'
+lastName : 'Liddell'
+age: 28
+Dans le même fichier créer object literal bob qui contiendra les informations suivantes:
+firstName: 'Bob'
+lastName: 'Lemon'
+age: 30
+Dans le même fichier créer object literal charlie qui contiendra les informations suivantes:
+firstName: 'Charlie'
+lastName: 'Charlot'
+age: 8
+Améliorer le programme précédent en ajoutant les fonctionnalités ci dessous.
+Faites preuve d'imagination et ajouter à chacun des objets une propriété language
+qui sera un tableau de string qui contiendra la liste de tous les langages de programmation que Alice,
+Bob et Charlie connaissent.
+*/
+
+/* OBJECT LITERAL */
+
 const alice = {
   firstName: 'Alice',
-  lastName: 'Liddel',
+  lastName: 'Liddell',
   age: 28,
-  language: ['Ruby, Shapir, Emeraude,']
+  language: ['Html', 'Php']
 }
 
 const bob = {
   firstName: 'Bob',
   lastName: 'Lemon',
   age: 30,
-  language: ['Js, Html, Solidity, Php, C++']
+  language: ['Perl', 'Python', 'Java']
 }
 
 const charlie = {
   firstName: 'Charlie',
   lastName: 'Charlot',
   age: 8,
-  language: ['Python, Cobra, Anaconda'],
-
+  language: ['NodeJS', 'Javascript', 'Sql']
 }
 
-const prinInfo = (objet) => {
-  console.log(`firstName: ${objet.firstName}`)
-  console.log(`lastName: ${objet.lastName}`)
-  console.log(`age: ${objet.age}`)
+
+/*
+Améliorer le programme précédent en ajoutant les fonctionnalités ci dessous.Créer une fonction printInfo
+qui prendra un objet en paramètre et qui affichera sur la console les informations concernant alice, bob ou charlie
+*/
+
+/* FONCTION PRINTINFO */
+
+const printInfo = (person) => {
+  console.log(`first name: ${person.firstName}`)
+  console.log(`last name: ${person.lastName}`)
+  console.log(`age: ${person.age}`)
 }
 
-const canVote = (objet) => {
-  if (objet.age >= 18) {
-    return true;
-  } else {
-    return false;
+printInfo(alice)
+printInfo(bob)
+printInfo(charlie)
+
+
+/*
+Améliorer le programme précédent en ajoutant les fonctionnalités ci dessous. Créer une fonction canVote
+qui prendra un objet en paramètre et qui retourne true si la personne peut voter ou false sinon.
+Par exemple canVote(alice) retournera true et canVote(charlie) retournera false.
+La majorité est à 18 ans.
+*/
+
+/* FONCTION CANVOTE */
+
+const canVote = (person) => {
+  return person.age > 18 ? true : false
+}
+
+console.log(canVote(alice))
+console.log(canVote(bob))
+console.log(canVote(charlie))
+
+
+/*
+Améliorer le programme précédent en ajoutant les fonctionnalités ci dessous. Ecrivez une fonction mostSkilledDev
+qui prendra comme paramètre 2 objets, parmi alice, bob et charlie et qui retournera le firstName de la personne 
+qui connait le plus de langages de programmation.
+En cas d'égalité draw sera retourné.
+*/
+
+/* FONCTION MOSTKILLEDDEV */
+
+const mostSkilledDev = (person1, person2) => {
+  if (person1.language.length > person2.language.length) {
+    return `${person1.firstName} à ${person1.language.length - person2.language.length} langage de plus que ${person2.firstName}`
+  }
+  else if (person1.language.length < person2.language.length) {
+    return `${person2.firstName} à ${person2.language.length - person1.language.length} langage de plus que ${person1.firstName}`
+  }
+  else {
+    return `draw: ${person1.firstName} connait le même nombre de langages que ${person2.firstName}`
   }
 }
 
-console.log(canVote(alice));
-console.log(canVote(bob));
-console.log(canVote(charlie));
-console.log();
-
-const mostSkilledDev = (obj1, obj2) => {
-  const nbLangP1 = obj1.language.length;
-  const nbLangP2 = obj2.language.length;
-  if (nbLangP1 > nbLangP2)
-    return obj1.firstName
-  else if (nbLangP1 < nbLangP2)
-    return obj2.firstName
-  else
-    return 'draw'
-}
-
-
-console.log(mostSkilledDev(alice, bob));
-console.log(mostSkilledDev(charlie, alice));
+console.log(mostSkilledDev(alice, bob))
+console.log(mostSkilledDev(alice, charlie))
+console.log(mostSkilledDev(bob, charlie))
+console.log(mostSkilledDev(bob, alice))
+console.log(mostSkilledDev(charlie, alice))
+console.log(mostSkilledDev(charlie, bob))
